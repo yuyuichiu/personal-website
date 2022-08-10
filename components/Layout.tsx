389241302bb8dd16f7f0b526a5cc92d9d37a5a6d
Dave Yu <yuichiuyu1915@gmail.com>
2022-08-10 10:12:54 +0800

    Edit meta description

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import { ReactNode } from "react"
 import webIcon from '../public/assets/moon.png'
 
+const description = "Dave Yu (俞睿釗), an experienced web developer who builds your project and evolves with technology.";
+
 interface LayoutProps {
   children?: ReactNode,
   title?: string,
@@ -13,7 +15,8 @@ const Layout : React.FC<LayoutProps> = (props) => {
   return <>
     <Head>
       <title>{props.title ? props.title : "Dave Yu | Full-Stack Web Developer"}</title>
-      <meta name="description" content="Dave Yu (俞睿釗), an experienced web developer who builds your project and evolves with technology." />
+      <meta name="description" content={description} />
+      <meta name="og:description" content={description} />
       <link rel="icon" href={webIcon.src} />
     </Head>
 
